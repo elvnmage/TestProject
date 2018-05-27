a983d68d9be40e6e72d718ab2da526494287d942
edemirbilek <edemirbilek@10.0.1.41>
2018-05-27 22:53:17 +0200

    blaaa

--- NewProject/src/hello.java
+++ NewProject/src/hello.java
@@ -1,4 +1,13 @@
 
+/**
+* @author <a href="mailto:akin@javaturk.org">Akin Kaldiroglu</a>
+*
+* For more info please @see <a href="http://www.javaturk.org">http://www.javaturk.org</a>
+*
+*/
 public class hello {
-
+ 
+   public String selamSoyle(String isim) {
+      return "Selam " + isim + " :)";
+   }
 }
